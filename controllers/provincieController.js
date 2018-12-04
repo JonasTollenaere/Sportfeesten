ba997da5770196125ac9bfedecf163d29df4e92c
JonasTollenaere <45364649+JonasTollenaere@users.noreply.github.com>
2018-12-04 20:29:29 +0100

    Update provincieController.js

--- controllers/provincieController.js
+++ controllers/provincieController.js
@@ -21,6 +21,3 @@ exports.provincie_detail = function (req, res, next) {
         res.render('provincie_detail', { title: 'Provincie Details', locatie_list: results.locatie_list, provincienaam: req.params.provincienaam });
     });
 };
-
-/// NOG IMPLEMENTEREN LATER: locatie_create_get, locatie_delete_get, locatie_delete_post, locatie_update_get, locatie_update_post
-
