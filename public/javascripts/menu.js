091b6b1744624f87bac4b1a6c860715e66462145
JonasTollenaere <45364649+JonasTollenaere@users.noreply.github.com>
2018-12-08 14:55:24 +0100

    Voorstel CSS
    Beoordeel maar of we hierop verder kunnen werken

---
+++ public/javascripts/menu.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    
+    var x = document.getElementById("SideBar");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+    
+}
